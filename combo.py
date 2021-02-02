a6c57c081e117a558659f403fdec394dadfb8ca6
Ahmed Ghanem <70071284+AHMEDGHANEM404@users.noreply.github.com>
2021-02-02 19:45:16 +0200

    Update combo.py

--- combo.py
+++ combo.py
@@ -4,7 +4,7 @@ import pyodbc
 import numpy as np
 from PyQt5.QtWidgets import QTableView, QTableWidget, QTableWidgetItem
 from PyQt5.QtSql import QSqlDatabase, QSqlQueryModel, QSqlQuery
-from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5.QtGui import QIcon, QStandardItemModel, QStandardItem
 from PyQt5.QtCore import Qt
 
 import sys
@@ -20,7 +20,7 @@ print(f'MS-ACCESS Drivers : {msa_drivers}')
 def createConnection():
     con_String = (
         r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
-        r'DBQ=C:\Users\ahmed\Desktop\filter.accdb;')
+        r'DBQ=E:\project\year\code\exe project\project v1.0\v1\filter.accdb;')
 
     global db
     db = QSqlDatabase.addDatabase('QODBC')
@@ -37,18 +37,18 @@ def createConnection():
 
 
 conn = pyodbc.connect(
-    r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
-    r'DBQ=C:\Users\ahmed\Desktop\filter.accdb;')
+        r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+        r'DBQ=E:\project\year\code\exe project\project v1.0\v1\filter.accdb;')
 
 cursor = conn.cursor()
 fac = []
 dep = []
 sub = []
 data = []
-# test = []
+level = []
 # select * from (faculty inner join department on faculty.fid =department.fid ) inner join subject on subject.did= department.did
 cursor.execute(
-    f'select fname,dname,subname from (faculty inner join department on faculty.fid =department.fid ) inner join subject on subject.did= department.did')
+       f'select  fname,dname,subname ,levels.name  from ((faculty inner join department on faculty.fid =department.fid ) inner join subject on subject.did= department.did)inner join levels on levels.ID =subject.levelid order by levels.ID')
 for row in cursor.fetchall():
     # print(row[0])
     fac.append(row[0])
@@ -56,7 +56,16 @@ for row in cursor.fetchall():
     dep.append(row[1])
     # print(row[2])
     sub.append(row[2])
+    print(row[3])
+    level.append(row[3])
+
+
+    
+# print(fac)
+# print(dep)
 # print(sub)
+print(level)
+
 
 
 class Ui_MainWindow(object):
@@ -75,10 +84,14 @@ class Ui_MainWindow(object):
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
         self.label_3.setGeometry(QtCore.QRect(210, 310, 47, 13))
         self.label_3.setObjectName("label_3")
+        # ظ================================================
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(210, 350, 47, 13))
+        self.label_4.setObjectName("label_4")
         self.comboBox_3 = QtWidgets.QComboBox(self.centralwidget)
         self.comboBox_3.setGeometry(QtCore.QRect(300, 300, 221, 22))
         self.comboBox_3.setObjectName("comboBox_3")
-        # self.comboBox_3.addItem("")
+        self.comboBox_3.addItem("")
         self.comboBox_3.addItem("")
         self.comboBox_4 = QtWidgets.QComboBox(self.centralwidget)
         self.comboBox_4.setGeometry(QtCore.QRect(300, 250, 221, 22))
@@ -90,6 +103,16 @@ class Ui_MainWindow(object):
         self.comboBox_5.setObjectName("comboBox_5")
         self.comboBox_5.addItem("")
         self.comboBox_5.addItem("")
+
+        # =======================================================
+        self.comboBox_6 = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox_6.setGeometry(QtCore.QRect(300, 350, 221, 22))
+        self.comboBox_6.setObjectName("comboBox_6")
+        self.comboBox_6.addItem("")
+        self.comboBox_6.addItem("")
+        self.comboBox_6.addItem("")
+        self.comboBox_6.addItem("")
+        # =====================================================
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(340, 380, 75, 23))
         self.pushButton.setObjectName("pushButton")
@@ -98,40 +121,55 @@ class Ui_MainWindow(object):
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
         self.pushButton.clicked.connect(self.pressed)
-
+        
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
+       
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "جامعة الازهر"))
         self.label.setText(_translate("MainWindow", "الكلية"))
         self.label_2.setText(_translate("MainWindow", "القسم "))
         self.label_3.setText(_translate("MainWindow", "المواد"))
-        x = -1
-        for i in fac:
+        self.label_4.setText(_translate("MainWindow", "الفرقة"))
+
+        x=0
+        for i in fac[2:]:
             self.comboBox_5.setItemText(x, _translate("MainWindow", i))
             x += 1
         self.comboBox_5.activated[str].connect(self.on_combobox_changed)
         self.comboBox_3.setItemText(0, _translate("MainWindow", ""))
         self.pushButton.setText(_translate("MainWindow", "بحث"))
-
+        
     def on_combobox_changed(self, value):
         print(value)
-        if value == fac[3]:
-
-            for i in dep[2:5]:
+        if value == fac[1]:
+            self.comboBox_4.clear()
+            for i in dep[:3]:
                 self.y.append(i)
                 print(i)
                 self.comboBox_4.addItem(i)
-            for i in sub:
+            self.comboBox_3.clear()
+            for i in sub[:3]:
                 self.comboBox_3.addItem(i)
+            self.comboBox_6.clear()
+            
+            for i in level[:3]:
+                self.comboBox_6.addItem(i)
+                
 
-        else:
-            for i in dep[:2]:
-                self.y.append(i)
-                print(i)
-                self.comboBox_4.addItem(i)
+        
+        if value==fac[3]:
+            self.comboBox_4.clear()
+            self.y.append(dep[3])
+            print(dep[3])
+            self.comboBox_4.addItem(dep[3])
+            self.comboBox_3.clear()
+            self.comboBox_3.addItem(sub[3])
+            self.comboBox_6.clear()
+            self.comboBox_6.addItem(level[3])
+            
 
     def pressed(self):
         print(self.comboBox_3.currentText())
@@ -139,13 +177,18 @@ class Ui_MainWindow(object):
         print(self.comboBox_4.currentText())
 
         print(self.comboBox_5.currentText())
-        qry = f"select distinct stu.name ,dname,fname from ((faculty inner join department on faculty.fid =department.fid ) inner join subject on subject.did= department.did) inner join stu on stu.facid =department.fid where department.dname='{self.comboBox_4.currentText()}'"
+
+        print(self.comboBox_6.currentText())
+
+
+        qry = f"select distinct stu.name ,dname,fname from ((faculty inner join department on faculty.fid =department.fid ) inner join subject on subject.did= department.did) inner join stu on stu.facid =department.fid where department.dname='{self.comboBox_4.currentText()}' and subject.subname ='{self.comboBox_3.currentText()}'"
+       
         self.w = displayData(qry)
         self.w.show()
 
-        for row in cursor.fetchall():
-            print(row)
-            data.append(row)
+        # for row in cursor.fetchall():
+        #     print(row)
+        #     data.append(row)
 
 
 def displayData(sqlStatement):
@@ -164,6 +207,7 @@ def displayData(sqlStatement):
 if __name__ == "__main__":
     createConnection()
     app = QtWidgets.QApplication(sys.argv)
+    app.setWindowIcon(QIcon("logo.jpg"))
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
